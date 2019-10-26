da3c45607174621fb494916d07b8256a70a91243
Vladyslav Tarasiuk <vtarasiu@student.unit.ua>
2019-10-26 11:18:40 +0300

    permission

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@ typedef struct		s_file
 	char			*path;
 	char			*full_path;
 	char			*link_dest;
+	char			permissions[11];
 	dev_t			dev_id;
 	mode_t			mode;
 	uid_t			uid;
@@ -73,6 +74,28 @@ char		*get_fullpath(char *path, char *basename)
 	return (ft_strdup(full_path));
 }
 
+void		get_permissions(t_file *file)
+{
+	char	*str;
+	mode_t	m;
+
+	str = file->permissions;
+	m = file->mode;
+	str[0] = get_type(mode);
+	str[1] = (mode & S_IRUSR) ? 'r' : '-';
+	str[2] = (mode & S_IWUSR) ? 'w' : '-';
+	str[3] = (mode & S_IXUSR) ? 'x' : '-';
+	str[4] = (mode & S_IRGRP) ? 'r' : '-';
+	str[5] = (mode & S_IWGRP) ? 'w' : '-';
+	str[6] = (mode & S_IXGRP) ? 'x' : '-';
+	str[7] = (mode & S_IROTH) ? 'r' : '-';
+	str[8] = (mode & S_IWOTH) ? 'w' : '-';
+	str[9] = (mode & S_IXOTH) ? 'x' : '-';
+	str[9] = (mode & S_ISVTX) ? 'T' : str[9];
+	str[10] = (mode) ? ' ' : '-';
+	str[11] = 0;
+}
+
 t_file		*filelist_new(struct stat *s, char *path, char *basename)
 {
 	t_file	*new;
@@ -91,7 +114,8 @@ t_file		*filelist_new(struct stat *s, char *path, char *basename)
 	new->atime = s.st_atim;
 	new->btime = (struct timespec){0};
 	new->ctime = s.st_ctim;
-	new->mtime = s.st_mtim;	
+	new->mtime = s.st_mtim;
+	get_permissions(new);
 	return (new);
 }
 
@@ -122,12 +146,16 @@ void		filelist_add(t_file **head, t_file **tail, t_file *to_add)
 
 void		filelist_print(t_file *head, t_flags flags)
 {
-	if ((flags & FLAG_LONG))
+	while (head)
 	{
-		;
-	};
-	else
-		ft_putendl(head->basename);
+		if ((flags & FLAG_LONG))
+		{
+			ft_putendl();
+		}
+		else
+			ft_putendl(head->basename);
+		head = head->next;
+	}
 }
 
 void		preprocess(char **argv, t_flags flags)
